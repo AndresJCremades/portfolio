f2ff7e0de6aa676d3a48c09af6ef086347c96eab
Andrés-Julián Cremades Monserrat <andres.j.cremades@gmail.com>
2018-09-06 15:45:53 +0200

    Edit Project Details

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+$(document).ready(function() {
+  $(".project-preview").on("click", function() {
+    $(this).siblings(".project-details").slideToggle();
+  });
+});
+
+var element = $(this).siblings(".project-details");
+// .is(":visible") will return true if the element is currently invisible
+if (element.is(":visible")) {
+  element.slideUp();
+} else {
+  element.slideDown();
+}
