52ead6e5a234d39133cc7699bff91eea3018f8a2
Andrés-Julián Cremades Monserrat <andres.j.cremades@gmail.com>
2018-09-07 13:33:14 +0200

    Create popup animation with jQuery to display project-details when hovering a project item

--- js/script.js
+++ js/script.js
@@ -1,26 +1,8 @@
-/*$(document).ready(function() {
-  $(".project-preview").on("click", function() {
-    $(this).siblings(".project-details").slideToggle();
-  });
-});
-
-var element = $(this).siblings(".project-details");
-// .is(":visible") will return true if the element is currently invisible
-if (element.is(":visible")) {
-  element.slideUp();
-} else {
-  element.slideDown();
-}
-*/
-
 $(document).ready(function() {
-  $(".project-preview").on("mouseover", function() {
-    $(this).siblings(".project-details").slideToggle();
-  });
-});
-
-$(document).ready(function() {
-  $(".project-preview").on("mouseout", function() {
-    $(this).siblings(".project-details").slideToggle();
-  });
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
 });
